e9d08d417388fdbc1ff78211b6bb5b3b531a6522
jokopriyono <joe.mandrake007@gmail.com>
2019-03-03 15:37:48 +0700

    Menambahkan view model untuk model user

--- app/src/main/java/com/jokopriyono/eudeka/mvvm/model/User.kt
+++ app/src/main/java/com/jokopriyono/eudeka/mvvm/model/User.kt
@@ -1,3 +1,3 @@
 package com.jokopriyono.eudeka.mvvm.model
 
-data class User(val email: String, val password: String)
+data class User(var email: String, var password: String)
---
+++ app/src/main/java/com/jokopriyono/eudeka/mvvm/viewmodel/UserViewModel.kt
@@ -0,0 +1,33 @@
+package com.jokopriyono.eudeka.mvvm.viewmodel
+
+import android.content.Context
+import android.text.TextUtils
+import com.jokopriyono.eudeka.mvvm.model.User
+import org.jetbrains.anko.toast
+
+class UserViewModel(val context: Context, val user: User) {
+    companion object {
+        const val TOAST_MESSAGE_SUCCESS: String = "Login Success"
+        const val TOAST_MESSAGE_FAILED: String = "Login Failed"
+    }
+
+    fun updateModel(email: String, password: String) {
+        user.email = email
+        user.password = password
+    }
+
+    fun validLogin() {
+        var isValid = true
+
+        if (TextUtils.isEmpty(user.email)) isValid = false
+        if (TextUtils.isEmpty(user.password)) isValid = false
+        if (isValid)
+            showToast(TOAST_MESSAGE_SUCCESS)
+        else
+            showToast(TOAST_MESSAGE_FAILED)
+    }
+
+    private fun showToast(message: String) {
+        context.toast(message)
+    }
+}
