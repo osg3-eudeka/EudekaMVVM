41aaeb05de906f8e3cbebd1376d39719a7f4ed91
jokopriyono <joe.mandrake007@gmail.com>
2019-03-07 22:49:04 +0700

    Menambahkan menu activity

---
+++ app/src/main/java/com/jokopriyono/eudeka/mvvm/view/menu/MenuActivity.kt
@@ -0,0 +1,13 @@
+package com.jokopriyono.eudeka.mvvm.view.menu
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.jokopriyono.eudeka.mvvm.R
+
+class MenuActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+}
